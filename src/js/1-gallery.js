8cc5650066869e0385680b9d8241cb76e3ec0dd5
annaK31 <148232742+annaK31@users.noreply.github.com>
2024-03-03 12:32:09 +0200

    js hw 9

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,5 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const images = [
   {
     preview:
@@ -83,3 +84,7 @@ function galleryItemEl(arr) {
     )
     .join('');
 }
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
